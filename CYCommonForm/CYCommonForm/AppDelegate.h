273039263203a879a0395b60948cf0e2928e52db
cy <q2522535@163.com>
2017-09-08 21:42:38 +0800

    create project

---
+++ CYCommonForm/CYCommonForm/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CYCommonForm
+//
+//  Created by cy on 08/09/2017.
+//  Copyright © 2017 com.jiemu.commonForm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CYCommonForm/CYCommonFormTests/CYCommonFormTests.m
@@ -0,0 +1,39 @@
+//
+//  CYCommonFormTests.m
+//  CYCommonFormTests
+//
+//  Created by cy on 08/09/2017.
+//  Copyright © 2017 com.jiemu.commonForm. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CYCommonFormTests : XCTestCase
+
+@end
+
+@implementation CYCommonFormTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
